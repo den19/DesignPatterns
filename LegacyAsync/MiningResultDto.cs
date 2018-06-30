d8ea03081f62f5b87ef9bb60928ee73f597f1ff0
Denis Kolesoff <den.kolesov@gmail.com>
2018-06-30 07:59:59 +0300

    LegacyAsync added

---
+++ LegacyAsync/MiningResultDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegacyAsync
+{
+    public class MiningResultDto
+    {
+        public string MiningText { get; set; }
+
+        public double ElapsedSeconds { get; set; }
+    }
+}
---
+++ LegacyAsync/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LegacyAsync
+{
+    class Program
+    {
+        static async Task MainAsync(string[] args)
+        {
+            //Console.WriteLine("Begin");
+            //var manager = new ApmManager();
+            //manager.BeginApmStyleMining($"https://asynccoinfunction.azurewebsites.net/api/asynccoin/4");
+            //Console.ReadLine();
+
+            //Console.WriteLine("start");
+            //var manager = new IntegrationManager();
+            //await manager.ExecuteAsync();
+            //Console.ReadLine();
+
+            //Console.WriteLine("start");
+            //var manager = new EapManager();
+            //manager.EapStyleMiningAsync(4);
+            //Console.ReadLine();
+
+            Console.WriteLine("start");
+            var manager = new IntegrationManager();
+            await manager.ExecuteAsync();
+            Console.ReadLine();
+        }
+    }
+}
